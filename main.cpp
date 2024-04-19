6c118ac8a2a7067b2439a4d2509adfdfb06e28f4
Juan Rojas <yo217@hotmail.es>
2024-04-19 05:16:47 -0500

    initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+void printMenu();
+
+int main() {
+    printMenu();
+  return 0;
+}
+
+void printMenu() {
+  std::cout << "===============" << std::endl;
+  std::cout << "+  MEMOCARDS  +" << std::endl;
+    std::cout << "===============" << std::endl;
+}
+
