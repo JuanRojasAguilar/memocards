e35d75b2221670af97f7e418326e1e73bc10bba5
Jose David Rojas Aguilar <josedrojasa@hotmail.com>
2024-04-21 02:29:01 +0000

    Fix pointers

--- include/subject_cards.hpp
+++ include/subject_cards.hpp
@@ -10,11 +10,11 @@ private:
   int cardCounter;
 
 public:
-  SubjectCards() { this->cardCounter = 0; }
+  SubjectCards() : cardCounter(0) {}
 
   void add_card(Card newCard) {
     if (cardCounter < 10) {
-      myCards.emplace_back(newCard);
+      myCards.push_back(newCard); // Use push_back to add the new card
       cout << "New card assigned" << endl;
       ++cardCounter;
     } else {
@@ -23,16 +23,19 @@ public:
   }
 
   void delete_card() {
-    if (myCards.size() > 0) {
+    if (!myCards.empty()) { // Check if myCards is not empty
       int card_index;
       cout << "Type the card position: " << endl;
       try {
         cin >> card_index;
-        myCards.erase(myCards.begin() + (card_index - 1));
-        --cardCounter;
-        cout << "Card has been deleted" << endl;
-
-      } catch (exception e) {
+        if (card_index >= 1 && card_index <= myCards.size()) {
+          myCards.erase(myCards.begin() + (card_index - 1));
+          --cardCounter;
+          cout << "Card has been deleted" << endl;
+        } else {
+          cout << "Invalid card position" << endl;
+        }
+      } catch (exception& e) { // Catch by reference to the exception
         cout << "Invalid input" << endl;
       }
     } else {
@@ -43,7 +46,7 @@ public:
   void show_cards() {
     if (!myCards.empty()) {
       for (size_t i = 0; i < myCards.size(); ++i) {
-        cout << (i + 1) << ". " << endl;
+        cout << (i + 1) << ". ";
         myCards[i].say_question();
       }
     } else {
